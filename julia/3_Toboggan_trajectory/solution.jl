2d818289ab3fdadaa40665a02c39e136316fc7db
michielstock <michielfmstock@gmail.com>
2020-12-03 14:53:12 +0100

    :wrench: find best path

--- julia/3_Toboggan_trajectory/solution.jl
+++ julia/3_Toboggan_trajectory/solution.jl
@@ -47,7 +47,7 @@ product_example = simulate_path(example, 1) *
                     simulate_path(example, 5) * 
                     simulate_path(example, 7) * 
                     simulate_path(example, 1, 2)
-@test  == 336
+@test product_example == 336
 
 space = read("data/3_Tobbogan_trajectory/input.txt", String) |> rstrip |> parse_input
 
@@ -58,3 +58,7 @@ product_directions = simulate_path(space, 1) *
                         simulate_path(space, 5) * 
                         simulate_path(space, 7) * 
                         simulate_path(space, 1, 2)
+
+# for fun, best direction and number of trees
+
+min_trees, best_dir = minimum((simulate_path(space, dir), dir) for dir in 1:size(space,1))
