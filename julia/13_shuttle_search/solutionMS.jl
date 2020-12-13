4cf1997b176925bc537164491f25f49dc6172643
michielstock <michielfmstock@gmail.com>
2020-12-13 19:22:24 +0100

    :star: found soluton day 13

--- julia/13_shuttle_search/solutionMS.jl
+++ julia/13_shuttle_search/solutionMS.jl
@@ -24,8 +24,33 @@ end
 time_to_wait(id, t) = (div(t, id) + 1) * id - t
 best_bus(ids, time) = minimum((time_to_wait(id, time), id) for id in skipmissing(ids))
 
+function inv_mod(N, b)
+    for x in 1:b
+        (N * x) % b == 1 && return x
+    end
+end
+
+function chinese_remainder(a, n)
+    @assert all(0 .≤ a .≤ n)
+    N = lcm(n...)
+    Ni = div.(N, n)
+    xi = inv_mod.(Ni, n)
+    return sum(a .* Ni .* xi) % N
+end
+
+function find_order_time(ids)
+    a = [i - 1  for (i, id) in enumerate(ids) if !ismissing(id)]
+    n = skipmissing(ids) |> collect
+    a .= a .* n .- a
+    a .%= n
+    return chinese_remainder(a, n)
+end
+
+
 input = read("data/13_shuttle_bus/input.txt", String)
 time, ids = parse_input(input)
 
 min_time, best_id = best_bus(ids, time) 
 sol1 = min_time * best_id
+
+sol2 = find_order_time(ids)
