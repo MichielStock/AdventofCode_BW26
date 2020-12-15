bcf5eafca759bf7c4e3e9f18da9d1a40d8595bdc
michielstock <michielfmstock@gmail.com>
2020-12-15 13:38:36 +0100

    :boom: day 14

--- julia/14_docking_data/solutionMS.jl
+++ julia/14_docking_data/solutionMS.jl
@@ -1,6 +1,6 @@
 #=
 Author: Michiel Stock
-AoC: day 13
+AoC: day 14
 
 Do weird things with masks.
 =#
@@ -9,27 +9,39 @@ Do weird things with masks.
 # d: default value
 # x: bit value
 
+using Combinatorics
+
 apply_mask(m, d, x) = (m & x) | (~m & d)
 
-input = """
+input1 = """
 mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
 mem[7] = 101
 mem[8] = 0
 """
 
-mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"
-m = parse_mask((c=='X' for c in mask))
-d = parse_mask((c=='1' for c in mask))
+input2 = """
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
+"""
+
+
 
 function parse_mask(mask)
     n = length(mask)
-    sum((2^(n-i) for (i, v) in enumerate(mask) if v))
+    reduce(+, (2^(n-i) for (i, v) in enumerate(mask) if v); init=0)
 end
 
-function process(input)
+mask = "000000000000000000000000000000X1001X"
+m = parse_mask((c=='X' for c in mask))
+d = parse_mask((c=='1' for c in mask))
+
+function process1(input)
     lines = split(rstrip(input), "\n")
     mem = Dict{Int,Int}()
+    ghost = Dict{Int,Int}()
     m, d = 0, 0
     for l in lines
         if l[2] == 'a' # new mask
@@ -45,8 +57,38 @@ function process(input)
     end
     return mem
 end
+
+function fill_all_positions!(mem, x, m, pos)
+    b = bitstring(m)
+    vals = [2^i for i in 0:36 if b[end-i]=='1']
+    for c in combinations(vals)
+        mem[pos+sum(c)] = x
+    end
+end
+
+function process2(input)
+    lines = split(rstrip(input), "\n")
+    mem = Dict{Int,Int}()
+    m, d = 0, 0
+    for l in lines
+        if l[2] == 'a' # new mask
+            mask = l[end-35:end]
+            m = parse_mask((c=='X' for c in mask))
+            d = parse_mask((c=='1' for c in mask))
+        else
+            matches = match(r"\[(\d+)\] = (\d+)", l)
+            pos = parse(Int, matches[1])
+            x = parse(Int, matches[2])
+            pos = ((~d & pos) | d) & ~m
+            mem[pos] = x
+            fill_all_positions!(mem, x, m, pos)
+        end
+    end
+    return mem
+end
+
     
 input = read("data/14_docking_data/input.txt", String)
-mem = process(input)
+sol1 = process1(input) |> values |> sum
 
-sol1 = sum(values(mem))
+sol2 = process2(input) |> values |> sum
