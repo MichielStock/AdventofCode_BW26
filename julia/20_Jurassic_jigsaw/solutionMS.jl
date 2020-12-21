0bef35993b5e1d39535860e23f936f038fbd28e8
michielstock <michielfmstock@gmail.com>
2020-12-21 13:18:51 +0100

    :tada: jigsaw

--- julia/20_Jurassic_jigsaw/solutionMS.jl
+++ julia/20_Jurassic_jigsaw/solutionMS.jl
@@ -186,15 +186,95 @@ end
 
 function get_image(pieces, connected)
     outline = get_outline(pieces, connected)
-    images = 0 #TODO: complete me
+    n, m = size(outline)
+    images = [pieces[outline[i,j]] for i in 1:n, j in 1:m]
+    # put top corner piece right
+    sec_corners = get_hashed_corners(images[2,1])
+    for piece in allconform(first(images))
+        if hash(piece[end,:]) in sec_corners
+            images[1,1] = piece
+            break
+        end
+    end
+    # now the first line
+    for i in 2:n
+        corner = (images[i-1,1][end,:])
+        for piece in allconform(images[i,1])
+            if corner == piece[1,:]
+                images[i,1] = piece
+                break
+            end
+        end
+    end
+    # now, all the rest
+    for i in 1:n
+        for j in 2:n
+            corner = (images[i,j-1][:,end])
+            for piece in allconform(images[i,j])
+                if corner == piece[:,1]
+                    images[i,j] = piece
+                    break
+                end
+            end
+        end
+    end
+    # return final image without border
+    pixels_piece = (size(images[1], 1) - 2)
+    n_pixels = n * pixels_piece
+    image = zeros(Bool, n_pixels, n_pixels)
+    for (i, p) in zip(1:n, 1:pixels_piece:n_pixels)
+        for (j, q) in zip(1:n, 1:pixels_piece:n_pixels)
+            image[p:p+pixels_piece-1, q:q+pixels_piece-1] .= images[i,j][2:end-1,2:end-1]
+        end
+    end
+    return image
+end
+
+
+
+seamonster = """
+                  # 
+#    ##    ##    ###
+ #  #  #  #  #  #   
+"""
+
+ function parse_seamonster(seamonster)
+    seamonster = split(rstrip(seamonster), "\n")
+    n, m = length(seamonster), maximum(length.(seamonster))
+    B = zeros(Bool, n, m)
+    for (i, l) in enumerate(seamonster)
+        for (j, c) in enumerate(l)
+            c == '#' && (B[i,j] = true)
+        end
+    end
+    return B
+end
+
+
+seamonster = parse_seamonster(seamonster)
+
+function detectobjects(image, seamonster)
+    n, m = size(image)
+    p, q = size(seamonster)
+    n_seamonsters = 0
+    C = CartesianIndices(image)
+    Q = CartesianIndices(seamonster) |> last
+    
+    I1, Ilast = first(C), last(C)
+    for conformation in allconform(image)
+        for I in I1:(Ilast-Q)
+            if all(conformation[I:I+Q-I1][seamonster])
+                n_seamonsters += 1
+            end
+        end
+    end
+    return n_seamonsters
 end
 
 
 
 
-
-
-#input = read("data/20_Jurassic_jigsaw/input.txt", String)
+input = read("data/20_Jurassic_jigsaw/input.txt", String)
 pieces = parse_input(input)
 
 connected = connecting(pieces)
@@ -202,3 +282,7 @@ connected = connecting(pieces)
 # product of ids at corners
 # i.e., just two connecting pieces
 sol1 = reduce(*, filter(id->length(connected[id])==2, keys(connected)))
+
+image = get_image(pieces, connected)
+n_seamonsters = detectobjects(image, seamonster)
+sol2 = sum(image) - n_seamonsters * sum(seamonster)
