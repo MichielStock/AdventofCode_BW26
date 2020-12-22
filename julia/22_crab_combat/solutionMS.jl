afb5ff1daff029c97e4507cc6b7409631b3bc6d1
michielstock <michielfmstock@gmail.com>
2020-12-22 18:04:37 +0100

    :crab: finish combat

--- julia/22_crab_combat/solutionMS.jl
+++ julia/22_crab_combat/solutionMS.jl
@@ -30,6 +30,39 @@ function playcombat(cards1, cards2)
     return score, nrounds
 end
 
+
+
+function recursivecombat(cards1, cards2)
+    played = Set{UInt}([])
+    while !isempty(cards1) && !isempty(cards2)
+        h = hash((cards1, cards2))
+        if h ∈ played
+            return true, 0
+        else
+            push!(played, h)
+        end
+        c1 = popfirst!(cards1)
+        c2 = popfirst!(cards2)
+        if (c1 ≤ length(cards1)) && (c2 ≤ length(cards2))
+            winner, score = recursivecombat(cards1[1:c1], cards2[1:c2])
+        else
+            winner = c1 > c2
+        end
+        if winner
+            push!(cards1, c1, c2)
+        else
+            push!(cards2, c2, c1)
+        end
+        
+    end
+    winner = length(cards1) > length(cards2)
+    cards = winner ? cards1 : cards2
+    n = length(cards)
+    score = (n:-1:1) .* (cards) |> sum
+    return winner, score
+end
+
 input = read("data/22_crab_combat/input.txt", String)
 cards1, cards2 = parse_input(input)
-score, nrounds = playcombat(cards1, cards2)
+score1, nrounds = playcombat(cards1, cards2)
+winner, score2 = recursivecombat(cards1, cards2)
