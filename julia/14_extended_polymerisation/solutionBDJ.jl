848ffda8213784e86a4ba49523798a9fe0cf8dd9
beramos <bram.de.jaegher@gmail.com>
2021-12-14 15:06:12 +0100

    fast solution of day 14

--- julia/14_extended_polymerisation/solutionBDJ.jl
+++ julia/14_extended_polymerisation/solutionBDJ.jl
@@ -4,31 +4,59 @@
 
 function parse_input(str)
   lines = split(str,"\n")
-  chain = lines[1]
-  rules = Dict([split(line," -> ")[1] => split(line," -> ")[2]|> only for line in lines[3:end]])
-  return collect(chain), rules
+  chain = lines[1] |> collect
+  polymer = Dict{String,Int}()
+  rules = Dict{String,Tuple}()
+  elements = Set{Char}()
+  
+  for i in 1:length(chain)-1 
+    pair = chain[i]*chain[i+1]
+    if !haskey(polymer, pair)
+      polymer[pair] = 1
+    else
+      polymer[pair] += 1
+    end
+  end
+
+  for line in lines[3:end]
+    pair = split(line," -> ")[1] |> collect
+    elements = union(elements, Set(pair))
+    insertion = split(line," -> ")[2] |> only
+    result = (pair[1]*insertion, insertion*pair[2], insertion)
+    rules[prod(pair)] = result
+  end
+  return polymer, rules, Dict([el => count(x -> x .== el, chain) for el in elements])
 end
 
-function find_and_insert!(chain, rules; N=1)
-  for step in 1:N
-    new_chain = copy(chain)
-    count = 0
-    for i in 1:length(chain)-1
-      if chain[i]*chain[i+1] in keys(rules)
-        insert!(new_chain, i+count+1,rules[chain[i]*chain[i+1]])
-        #println("Inserting $(rules[chain[i]*chain[i+1]]) at $(i+count+1)")
-        #println(new_chain)
-        count += 1
+function step!(polymer, rules, tracker)
+  new_polymer = copy(polymer)
+  for (pair,num_of_insertions) in polymer
+    if haskey(rules, pair) && (num_of_insertions > 0)
+      new_pairs = rules[pair][1:2]
+      println(pair)
+      println(new_pairs)
+      insertion = rules[pair][end]
+      tracker[insertion] += num_of_insertions
+      new_polymer[pair] -= num_of_insertions 
+      
+      for new_pair in new_pairs
+        if haskey(new_polymer, new_pair)
+          new_polymer[new_pair] += num_of_insertions
+        else
+          new_polymer[new_pair] = num_of_insertions
+        end
       end
     end
-    chain = new_chain
-  end
-  return chain
+  end 
+  polymer = copy(new_polymer) 
+  return polymer, tracker
 end
 
-function output(chain)
-  histo = [sum(el .== chain) for el in unique(chain) |> collect] |> sort
-  histo[end]-histo[1]
+function step!(polymer, rules, tracker, N)
+  for i in 1:N
+    polymer, tracker = step!(polymer, rules, tracker)
+  end
+  return polymer,tracker
 end
 
 
@@ -55,48 +83,27 @@ CN -> C"""
 
 # --- Part 1 --- #
 # Test
-chain, rules = parse_input(example)
-chain = find_and_insert!(chain, rules; N=10) |> output
+polymer, rules, tracker = parse_input(example)
+polymer, tracker = step!(polymer, rules, tracker, 10)
+values(tracker) |> collect |> sort |> x->x[end]-x[1]
 
 # Full
 raw_input = read("./data/14_extended_polymerisation/input.txt", String) 
-chain, rules = parse_input(raw_input)
-chain = find_and_insert!(chain, rules; N=10) |> output
+polymer, rules, tracker = parse_input(raw_input)
+polymer, tracker = step!(polymer, rules, tracker, 10)
+values(tracker) |> collect |> sort |> x->x[end]-x[1]
 
+
+# --- Part 2 --- #
+# Test
+polymer, rules, tracker = parse_input(example)
+polymer, tracker = step!(polymer, rules, tracker, 40)
+values(tracker) |> collect |> sort |> x->x[end]-x[1]
+
+# Full
 raw_input = read("./data/14_extended_polymerisation/input.txt", String) 
-chain, rules = parse_input(raw_input)
-chain = find_and_insert!(chain, rules; N=40) |> output
+polymer, rules, tracker = parse_input(raw_input)
+polymer, tracker = step!(polymer, rules, tracker, 40)
+values(tracker) |> collect |> sort |> x->x[end]-x[1]
 
 
-function polymerise!(chain; max_iter = 1000)
-  changed = true
-  iter = 0
-  while changed & (iter < max_iter)
-    old_chain = copy(chain)
-    chain = find_and_insert!(chain)
-    changed = length(old_chain) != length(chain)
-    println()
-    iter += 1
-    old_chain = chain
-  end
-  return chain
-end
-
-rules = Dict(
-  "CH" => 'B',
-  "HH" => 'N',
-  "CB" => 'H',
-  "NH" => 'C',
-  "HB" => 'C',
-  "HC" => 'B',
-  "HN" => 'C',
-  "NN" => 'C',
-  "BH" => 'H',
-  "NC" => 'B',
-  "NB" => 'B',
-  "BN" => 'B',
-  "BB" => 'N',
-  "BC" => 'B',
-  "CC" => 'N',
-  "CN" => 'C'
-)
